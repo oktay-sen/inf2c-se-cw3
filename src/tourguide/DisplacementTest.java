524dfe8a4760407b1de33dcbb82a40da42661417
Oktay Sen <senokt.16@gmail.com>
2017-11-19 18:57:20 +0000

    Added tests for Displacement

--- src/tourguide/DisplacementTest.java
+++ src/tourguide/DisplacementTest.java
@@ -23,5 +23,66 @@ public class DisplacementTest {
         assertEquals(0.0, bearing, EPS);
     }
 
-    //TODO: Write more tests.
+    @Test
+    public void testSouthBearing() {
+        double bearing = new Displacement(0.0, -1.0).bearing();
+        assertEquals(180.0, bearing, EPS);
+    }
+
+    @Test
+    public void testEastBearing() {
+        double bearing = new Displacement(1.0, 0.0).bearing();
+        assertEquals(90.0, bearing, EPS);
+    }
+
+    @Test
+    public void testWestBearing() {
+        double bearing = new Displacement(-1.0, 0.0).bearing();
+        assertEquals(270.0, bearing, EPS);
+    }
+
+    @Test
+    public void testNEBearing() {
+        double bearing = new Displacement(1.0, 1.0).bearing();
+        assertEquals(45.0, bearing, EPS);
+    }
+    @Test
+    public void testSEBearing() {
+        double bearing = new Displacement(1.0, -1.0).bearing();
+        assertEquals(135.0, bearing, EPS);
+    }
+    @Test
+    public void testNWBearing() {
+        double bearing = new Displacement(-1.0, 1.0).bearing();
+        assertEquals(315.0, bearing, EPS);
+    }
+    @Test
+    public void testSWBearing() {
+        double bearing = new Displacement(-1.0, -1.0).bearing();
+        assertEquals(225.0, bearing, EPS);
+    }
+
+    @Test
+    public void testDistance1() {
+        double distance = new Displacement(3.0, 4.0).distance();
+        assertEquals(5.0, distance, EPS);
+    }
+
+    @Test
+    public void testDistance2() {
+        double distance = new Displacement(-3.0, 4.0).distance();
+        assertEquals(5.0, distance, EPS);
+    }
+
+    @Test
+    public void testDistance3() {
+        double distance = new Displacement(3.0, -4.0).distance();
+        assertEquals(5.0, distance, EPS);
+    }
+
+    @Test
+    public void testDistance4() {
+        double distance = new Displacement(-3.0, -4.0).distance();
+        assertEquals(5.0, distance, EPS);
+    }
 }
