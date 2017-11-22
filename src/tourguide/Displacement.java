97f5c1f016f5868e317b7fb1946662d59b6dd88d
Oktay Sen <senokt.16@gmail.com>
2017-11-22 08:30:45 +0000

    Added javadoc comments for Displacement

--- src/tourguide/Displacement.java
+++ src/tourguide/Displacement.java
@@ -2,26 +2,49 @@ package tourguide;
 
 import java.util.logging.Logger;
 
+/**
+ * A class that represents the displacement between two points.
+ * Contains methods that do two-dimentional calculations on this displacement.
+ */
 public class Displacement {
     private static Logger logger = Logger.getLogger("tourguide");
-       
+
+    /**
+     * The amount of displacement due east.
+     */
     public double east;
+
+    /**
+     * The amount of displacement due north.
+     */
     public double north;
-    
+
+    /**
+     * Creates a displacement object given the difference between two points' coordinates.
+     * @param e The difference between the two points' eastings.
+     * @param n The difference between the two points' northings.
+     */
     public Displacement(double e, double n) {
         logger.finer("East: " + e + "  North: "  + n);
         
         east = e;
         north = n;
     }
-    
+
+    /**
+     * Calculates the distance covered by this displacement.
+     * @return The distance covered.
+     */
     public double distance() {
         logger.finer("Entering");
         
         return Math.sqrt(east * east + north * north);
     }
-    
-    // Bearings measured clockwise from north direction.
+
+    /**
+     * Calculates the bearing of the displacement.
+     * @return The bearing in degrees, starting from North, clockwise.
+     */
     public double bearing() {
         logger.finer("Entering");
               
